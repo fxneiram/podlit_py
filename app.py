2814065b12f6e52d34f1742fd785fdcec8006ab9
Faver Xavier Neira Molina <fxneiram@gmail.com>
2024-09-21 14:35:26 -0500

    fix

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from tk.task_queue_manager import TaskQueueManager
+from tk.window_task_queue_manager import WindowTaskQueueManager
 import tkinter as tk
 
 
 class App:
     def __init__(self, root):
         self.root = root
-        self.task_queue_app = TaskQueueManager(self.root)
+        self.task_queue_app = WindowTaskQueueManager(self.root)
     
     def run(self):
         self.task_queue_app.run()
